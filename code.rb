d596140d7d8897887ccd1ed9b4e3bdd67429f405
Constea <constantinarabadjiu@gmail.com>
2022-11-28 21:41:57 +0200

    Modified the play function

--- code.rb
+++ code.rb
@@ -1,22 +1,24 @@
 class Board
+    attr_reader :sign, :sign_round
+
     def initialize
-        @cell_1 = '-'
-        @cell_2 = '-'
-        @cell_3 = '-'
-        @cell_4 = '-'
-        @cell_5 = '-'
-        @cell_6 = '-'
-        @cell_7 = '-'
-        @cell_8 = '-'
-        @cell_9 = '-'
+        @cell1 = '-'
+        @cell2 = '-'
+        @cell3 = '-'
+        @cell4 = '-'
+        @cell5 = '-'
+        @cell6 = '-'
+        @cell7 = '-'
+        @cell8 = '-'
+        @cell9 = '-'
         @sign = 'x'
         @sign_round = 0
     end
 
     def create_board
-        puts line_1 = ["#{@cell_1}|#{@cell_2}|#{@cell_3}"]
-        puts line_2 = ["#{@cell_4}|#{@cell_5}|#{@cell_6}"]
-        puts line_3 = ["#{@cell_7}|#{@cell_8}|#{@cell_9}"]
+        puts line1 = ["#{@cell1}|#{@cell2}|#{@cell3}"]
+        puts line2 = ["#{@cell4}|#{@cell5}|#{@cell6}"]
+        puts line3 = ["#{@cell7}|#{@cell8}|#{@cell9}"]
     end
 
     def mark_cell(cell_number)
@@ -28,53 +30,54 @@ class Board
 
         case cell_number
         when 1
-            @cell_1 = @sign
+            @cell1 = @sign
         when 2
-            @cell_2 = @sign
+            @cell2 = @sign
         when 3
-            @cell_3 = @sign
+            @cell3 = @sign
         when 4
-            @cell_4 = @sign
+            @cell4 = @sign
         when 5
-            @cell_5 = @sign
+            @cell5 = @sign
         when 6
-            @cell_6 = @sign
+            @cell6 = @sign
         when 7
-            @cell_7 = @sign
+            @cell7 = @sign
         when 8
-            @cell_8 = @sign
+            @cell8 = @sign
         when 9
-            @cell_9 = @sign
+            @cell9 = @sign
         end
         self.create_board
         @sign_round += 1
     end
 
     def calculate_winner
-        if (@cell_1 == 'x' && @cell_2 == 'x' && @cell_3 == 'x') || (@cell_1 == 'o' && @cell_2 == 'o' && @cell_3 == 'o')
-            puts 'You win'
-        elsif (@cell_4 == 'x' && @cell_5 == 'x' && @cell_6 == 'x') || (@cell_4 == 'o' && @cell_5 == 'o' && @cell_6 == 'o')
-            puts 'You win'
-        elsif (@cell_7 == 'x' && @cell_8 == 'x' && @cell_9 == 'x') || (@cell_7 == 'o' && @cell_8 == 'o' && @cell_9 == 'o')
-            puts 'You win'
-        elsif (@cell_1 == 'x' && @cell_4 == 'x' && @cell_7 == 'x') || (@cell_1 == 'o' && @cell_4 == 'o' && @cell_7 == 'o')
-            puts 'You win'
-        elsif (@cell_2 == 'x' && @cell_5 == 'x' && @cell_8 == 'x') || (@cell_2 == 'o' && @cell_5 == 'o' && @cell_9 == 'o')
-            puts 'You win'
-        elsif (@cell_3 == 'x' && @cell_6 == 'x' && @cell_9 == 'x') || (@cell_3 == 'o' && @cell_6 == 'o' && @cell_9 == 'o')
-            puts 'You win'
-        elsif (@cell_1 == 'x' && @cell_5 == 'x' && @cell_9 == 'x') || (@cell_1 == 'o' && @cell_5 == 'o' && @cell_9 == 'o')
-            puts 'You win'
-        elsif (@cell_3 == 'x' && @cell_5 == 'x' && @cell_7 == 'x') || (@cell_3 == 'o' && @cell_5 == 'o' && @cell_7 == 'o')
-            puts 'You win'
+        if (@cell1 == 'x' && @cell2 == 'x' && @cell3 == 'x') || (@cell1 == 'o' && @cell2 == 'o' && @cell3 == 'o')
+            return 'Win'
+        elsif (@cell4 == 'x' && @cell5 == 'x' && @cell6 == 'x') || (@cell4 == 'o' && @cell5 == 'o' && @cell6 == 'o')
+            return 'Win'
+        elsif (@cell7 == 'x' && @cell8 == 'x' && @cell9 == 'x') || (@cell7 == 'o' && @cell8 == 'o' && @cell9 == 'o')
+            return 'Win'
+        elsif (@cell1 == 'x' && @cell4 == 'x' && @cell7 == 'x') || (@cell1 == 'o' && @cell4 == 'o' && @cell7 == 'o')
+            return 'Win'
+        elsif (@cell2 == 'x' && @cell5 == 'x' && @cell8 == 'x') || (@cell2 == 'o' && @cell5 == 'o' && @cell9 == 'o')
+            return 'Win'
+        elsif (@cell3 == 'x' && @cell6 == 'x' && @cell9 == 'x') || (@cell3 == 'o' && @cell6 == 'o' && @cell9 == 'o')
+            return 'Win'
+        elsif (@cell1 == 'x' && @cell5 == 'x' && @cell9 == 'x') || (@cell1 == 'o' && @cell5 == 'o' && @cell9 == 'o')
+            return 'Win'
+        elsif (@cell3 == 'x' && @cell5 == 'x' && @cell7 == 'x') || (@cell3 == 'o' && @cell5 == 'o' && @cell7 == 'o')
+            return 'Win'
         else
-            puts 'You lost'
+            return 'Lose'
         end
     end
 end
 
 class Player
     attr_accessor :name, :sign
+    
     def initialize(name, sign)
         @name = name
         @sign = sign
@@ -82,27 +85,39 @@ class Player
 end
 
 class Game
-    attr_reader :player_1
-
-    def initialize(player_1, player_2, board)
-        @player_1 = player_1
-        @player_2 = player_2
+    def initialize(player1, player2, board)
+        @player1 = player1
+        @player2 = player2
         @board = board
     end
 
-    def play
-        puts 'Choose the name for player 1'
-        @player_1.name = gets
-        puts 'Choose your sign'
-        @player_1.sign = gets
-        puts 'Choose the name for player 2'
-        @player_2.name = gets
-        puts 'Choose your sign'
-        @player_2.sign = gets
-        puts @player_1.name, @player_2.name
+    def player_selection
+        puts 'Choose the name for player 1 :'
+        @player1.name = gets.chomp
+        puts 'Choose the name for player 2 :'
+        @player2.name = gets.chomp
     end
 
+    def play
+        @board.create_board
+        puts 'Introduce the number of the cell, from 1 to 9'
+        9.times do
+            if @board.sign_round % 2 == 0
+                puts "#{@player1.name}, write the cell number :"
+            else
+                puts "#{@player2.name}, write the cell number :"
+            end
+            cell_number_input = gets.chomp
+            @board.mark_cell(cell_number_input.to_i)
+            if @board.calculate_winner == 'Win'
+                puts 'You win'
+                break
+            end
+        end
+        puts 'You lost' if @board.calculate_winner == 'Lose'
+    end
 end
    
 game = Game.new(Player.new('first', 'x'), Player.new('second', 'o'), Board.new)
+game.player_selection
 game.play
